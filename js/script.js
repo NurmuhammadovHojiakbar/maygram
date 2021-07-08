f0dcae610f7154ec18275c1ad566b81c275745a3
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-07-08 10:42:37 +0500

    Message sending

--- js/script.js
+++ js/script.js
@@ -12,3 +12,56 @@ registerForm.addEventListener("submit", function(evt){
     
     registerContainer.style.display = "none"
 })
+
+
+let messageListUser = document.querySelector(".message-list__user")
+let messageListRobot= document.querySelector(".message-list__robot")
+let messageInputUser= document.getElementById("user-message")
+let messageInputRobot= document.getElementById("robot-message")
+
+
+document.querySelector(".user-form").onsubmit = function(a){
+    a.preventDefault();
+
+    let newUserMessage = document.createElement("li")
+
+    newUserMessage.textContent = messageInputUser.value
+    
+    newUserMessage.classList.add("message-item")
+    newUserMessage.classList.add("message-item__left")
+    
+    messageListUser.append(newUserMessage)
+    
+    let newRobotMessage = document.createElement("li")
+    
+    newRobotMessage.textContent = messageInputUser.value
+    
+    newRobotMessage.classList.add("message-item")
+    newRobotMessage.classList.add("message-item__left")
+    
+    messageListRobot.append(newRobotMessage)
+    messageInputUser.value = ""
+}
+
+document.querySelector(".robot-form").onsubmit = function(a){
+    a.preventDefault();
+
+    let newUserMessage = document.createElement("li")
+
+    newUserMessage.textContent = messageInputRobot.value
+    
+    newUserMessage.classList.add("message-item")
+    newUserMessage.classList.add("message-item__right")
+    
+    messageListUser.append(newUserMessage)
+    
+    let newRobotMessage = document.createElement("li")
+    
+    newRobotMessage.textContent = messageInputRobot.value
+    
+    newRobotMessage.classList.add("message-item")
+    newRobotMessage.classList.add("message-item__right")
+    
+    messageListRobot.append(newRobotMessage)
+    messageInputRobot.value = ""
+}
